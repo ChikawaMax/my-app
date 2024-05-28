a664dd4a5b68bce85697492b34665b0669d55c87
本間敬太 <keitakunv2@gmail.com>
2024-05-28 23:29:59 +0900

    ✨ useStateで表示非表示ボタン実装

--- src/app/component/backcolor.tsx
+++ src/app/component/backcolor.tsx
@@ -3,31 +3,49 @@
 import { useCallback, useEffect, useState } from "react";
 
 
-export  const MyButton=() =>{
+export const MyButton = () => {
 
-  const [count,setCount]=useState(1)
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
-  const handleClick=useCallback((e:any)=>{
-    console.log(e.target);
-    if(count<10){
-      setCount((count)=>count+1);
+
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
     }
-  },[count]);
+  }, [count]);
 
-  useEffect(()=>{    
-    document.body.style.backgroundColor="red";
-    return()=>{
-      document.body.style.backgroundColor="";
+  useEffect(() => {
+    document.body.style.backgroundColor = "red";
+    return () => {
+      document.body.style.backgroundColor = "";
     }
-  },[]);
+  }, []);
 
-  return(
+  const handleChange = useCallback((e: any) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください！");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, [])
+
+  return (
     <div>
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
         onClick={handleClick}
       >ボタン</button>
+      <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+        onClick={handleDisplay}
+      >{isShow ? "非表示" : "表示"}</button>
 
+      <input className="bg-blue-700" type="text" value={text} onChange={handleChange} />
     </div>
   );
 }
