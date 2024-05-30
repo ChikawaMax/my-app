8f460a80176d6d2c3830806fac243d8f4d2f3718
本間敬太 <keitakunv2@gmail.com>
2024-05-31 06:55:01 +0900

    🎨 リファクタリング

--- src/app/component/main.tsx
+++ src/app/component/main.tsx
@@ -31,18 +31,7 @@ const ITEMS = [{
 },
 ]
 
-type Object = {
-  name: string
-  age: number
-  color: string
-}
-
-type Props = {
-  name: string;
-  obj: Object;
-}
-
-export const Main = ({ name, obj }: Props) => {
+export const Main = (Props: any) => {
 
   const [items, setItems] = useState(ITEMS)
   const handleReduce = useCallback(() => {
@@ -56,10 +45,10 @@ export const Main = ({ name, obj }: Props) => {
       <div>{items.length}</div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          {name} Page
+          {Props.name} Page
         </p>
         <p>
-          {obj.name}は{obj.age}歳で{obj.color}色です。
+          {Props.obj.name}は{Props.obj.age}歳で{Props.obj.color}色です。
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
